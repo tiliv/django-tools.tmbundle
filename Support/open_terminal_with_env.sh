9a31e889d5c0de42b468fb29f37b0de4a761aed5
Tim Valenta <tonightslastsong@gmail.com>
2013-02-03 00:28:34 -0700

    Fixes issue with launching windows instead of tabs
    The "cd" command of the `open_terminal_with_env.sh` script was issued without the "in window 0" context.

--- Support/open_terminal_with_env.sh
+++ Support/open_terminal_with_env.sh
@@ -14,7 +14,10 @@ osascript <<-APPLESCRIPT
 	tell app "Terminal"
 	    launch
 	    activate
-	    do script "cd $(esc "${TM_PROJECT_DIRECTORY}")"
+        tell application "System Events"
+            keystroke "t" using {command down}
+        end tell
+	    do script "cd $(esc "${TM_PROJECT_DIRECTORY}")" in window 0
         do script "VIRTUALENV=$VIRTUALENV VIRTUALENVWRAPPER=$VIRTUALENVWRAPPER source '$TM_BUNDLE_SUPPORT/activate_env.sh'" in window 0
         do script "$("$TM_BUNDLE_SUPPORT"/export_variables.sh $EXPORT_VARIABLES_TO_TERMINAL)" in window 0
         do script "clear" in window 0
