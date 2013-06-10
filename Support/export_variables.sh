647cb1743210bed7a740d10cd631653e8cbb4fe3
Tim Valenta <tonightslastsong@gmail.com>
2013-06-09 22:19:55 -0700

    Adds always-on export of DJANGO_ variables

--- Support/export_variables.sh
+++ Support/export_variables.sh
@@ -11,6 +11,10 @@ RUBY
 
 if [ $1 ]; then
     # Echoes back the "export" names beginning with $1
+    for var in $(eval echo "\${!DJANGO_@}")
+    do
+        echo "export $var=$(esc "${!var}")"
+    done
     for var in $(eval echo "\${!$1@}")
     do
         echo "export $var=$(esc "${!var}")"
